af3ebda8c763b8e3dc51ad8659c19b2e7e4550e8
Joshua <42632439+joshiefu@users.noreply.github.com>
2022-04-09 20:33:38 +1200

    Add graphql playground

--- back-end/go.mod
+++ back-end/go.mod
@@ -2,4 +2,8 @@ module github.com/jardinehc/but-whats-that-mean
 
 go 1.16
 
-require github.com/graph-gophers/graphql-go v1.3.0
+require (
+	github.com/99designs/gqlgen v0.17.2
+	github.com/graph-gophers/graphql-go v1.3.0
+	github.com/sirupsen/logrus v1.8.1
+)
--- back-end/main.go
+++ back-end/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	_ "embed"
-	"log"
 	"net/http"
 
+	"github.com/99designs/gqlgen/graphql/playground"
 	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
+	log "github.com/sirupsen/logrus"
 )
 
 //go:embed schema.graphql
@@ -20,5 +21,8 @@ func main() {
 
 	schema := graphql.MustParseSchema(schema, &resolver{})
 	http.Handle("/query", &relay.Handler{Schema: schema})
+	http.Handle("/", playground.Handler("playground", "/query"))
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Info("listening on port 8080...")
+
 }
